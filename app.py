7488bc4154333eb84ee18e552ccd24a1b23cd1c4
Shubham <gaikwadshubham65@gmail.com>
2025-03-16 22:57:45 +0530

    latest code added

--- app.py
+++ app.py
@@ -5,12 +5,19 @@ from pydantic import BaseModel
 import numpy as np
 import pickle
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import date
+import json
 
 with open("model.pkl", "rb") as file_obj:
     model = pickle.load(file_obj)
 
 with open("scaler.pkl", "rb") as file_obj:
     scaler = pickle.load(file_obj)
+
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+fraud_db = pd.read_csv("fraud_upi_list.csv")
 
 class UpiData(BaseModel):
     transDay: int	
@@ -50,6 +57,39 @@ def UpiFraudPredictionEndpoint(data:UpiData):
       return {
                 'prediction': status
             }
+
+@app.get('/get-upi-data')
+def getUpiDataEndpoint():
+    todays_date = date.today()
+    predict=[]
+    for item in data:
+        test = pd.DataFrame([{
+            'trans_day': todays_date.day,'trans_month': todays_date.month,
+            'trans_year': todays_date.year,'upi_number': item["Phone Number"],'trans_amount': item["Amount"]
+        }])
+        scaleTest=scaler.transform(test)
+        result=model.predict(scaleTest)
+        if(result==[1]):
+            status="FRAUD"
+        else:
+            status="NOT FRAUD"
+        predictionData = {"upi_number":item["Phone Number"],"trans_amount": item["Amount"],"status":status}
+        predict.append(predictionData)
+      
+    return {
+        'data': predict
+    }
+
+
+class UPIRequest(BaseModel):
+    upi_id: str
+
+@app.post("/check_upi")
+async def check_upi(data: UPIRequest):
+    upi_id = data.upi_id.strip()
+    is_fraud = upi_id in fraud_db["upi_id"].values
+    return {"upi_id": upi_id, "fraud": is_fraud}
+
 
 if __name__ == '__main__':
     uvicorn.run("app:app", host='127.0.0.1', port=8000,reload=True)
